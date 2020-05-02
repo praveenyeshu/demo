3fe1f47aa97a1b0da8b0b8c94e73d2a12a9b6d87
praveenyeshu <63011797+praveenyeshu@users.noreply.github.com>
2020-05-02 23:17:29 +0530

    update app name

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/food-ordering-web/demo'));
+app.use(express.static(__dirname + '/food-ordering-web/food-ordering-web'));
 
 app.get('/*', function(req,res) {
     
